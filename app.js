ee5eb78cc787d00aa55256f39399834510f9a352
Brent <brentthomas.c@gmail.com>
2017-08-27 21:40:49 -0600

    ahhh

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ mongoose.connect(config.uri, {useMongoClient: true}, (err) => {
 
 
 // Body Parser
-
 // app.use(function (req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
 //   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
